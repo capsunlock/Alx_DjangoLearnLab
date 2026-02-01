b5f530ceab145c6e95996811faf2c8d65b5b2d29
capsunlock <capsonluck@gmail.com>
2026-02-01 10:53:20 +0100

    04

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,23 +1,18 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view
-from . import views
+from . import views  # Use this to keep everything consistent
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    # FBV URL
-    path('books/', list_books, name='list_books'),
-    # CBV URL - Note the use of .as_view() and <int:pk>
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-
-    # Authentication URLs
-    # We specify the template_name so Django knows where to look
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
-    path('admin_view/', admin_view, name='admin_view'),
-    path('librarian_view/', librarian_view, name='librarian_view'),
-    path('member_view/', member_view, name='member_view'),
-
+    path('admin_view/', views.admin_view, name='admin_view'),
+    path('librarian_view/', views.librarian_view, name='librarian_view'),
+    path('member_view/', views.member_view, name='member_view'),
+    
+    # Secure views for the task
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,8 @@ from .models import Book
 from .models import Library
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import user_passes_test
 
 # 1. Function-based view to list all books
 def list_books(request):

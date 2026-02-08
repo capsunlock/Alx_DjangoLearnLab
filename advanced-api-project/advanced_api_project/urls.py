3a6e227613354f53c989ebf308dc46563667a091
capsunlock <capsonluck@gmail.com>
2026-02-08 22:53:07 +0100

    1. Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Change 'view' to 'urls'
+    path('admin/', admin.site.urls), 
+    
+    path('api/', include('api.urls')),
+    
+    # This enables the "Log in" button in the top right of your browser
+    path('api-auth/', include('rest_framework.urls')), 
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+
+"""
+BookListView:
+- Purpose: Provides a read-only list of all books.
+- Access: Unauthenticated users allowed.
+
+BookCreateView:
+- Purpose: Allows creating a new book instance.
+- Validation: Uses BookSerializer to ensure years aren't in the future.
+- Access: Restricted to authenticated users.
+"""
+
+# Listview: Retrieve all books. Accessible to everyone.
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny] # Explicitly open to all
+
+# DetailView: Retrieve a single book by ID. Accessible to everyone.
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+# CreateView: Add a new book. Authenticated users only.
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# UpdateView: Modify an existing book. Authenticated users only.
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# DeleteView: Remove a book. Authenticated users only.
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
